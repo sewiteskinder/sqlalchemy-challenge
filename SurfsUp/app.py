2a6b6f726e0e19bb50f7639391ae1a031e177eae
sewiteskinder <sewit.eskinder@gmail.com>
2023-03-08 00:15:18 -0600

    api static routes complete

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -41,8 +41,54 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/tobs<start><br/>"
-        f"/api/v1.0/tobs<start>/<end>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
-if __name__ == "__main__":
+@app.route("/api/v1.0/precipitation")
+def date():
+
+    session = Session(engine)
+
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+            filter(Measurement.date >= year_ago).all()
+    
+    session.close()
+
+    prcp = dict(results)
+
+    return jsonify(prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    session = Session(engine)
+
+    results = session.query(Measurement.station).\
+        group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+
+    session.close()
+
+    station_names = list(np.ravel(results))
+
+    return jsonify(station_names)
+
+@app.route("/api/v1.0/tobs")
+def dates():
+
+    session = Session(engine)
+
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281', Measurement.date >= year_ago).all()
+
+    session.close()
+
+    temp_obv = list(np.ravel(results))
+
+    return jsonify(temp_obv)
+
+if __name__ == '__main__':
     app.run(debug=True)
